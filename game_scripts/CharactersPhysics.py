8cba5ef24e979b5ea9053674c1e2a0f5d23bd2c5
Artemy <artemy.jevlak@yandex.ru>
2024-01-07 07:21:33 +0300

    Добавил изменение в статическом положении персонажа в зависимости от выбранного направления

--- game_scripts/CharactersPhysics.py
+++ game_scripts/CharactersPhysics.py
@@ -14,10 +14,12 @@ class Hero(pygame.sprite.Sprite):
                                                        f"{self.index_of_hero_static_img}.png", -1), (70, 90))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 300, 300
+        self.is_left = False
         self.dx = 1
         self.dy = 1
 
     def do_the_running_animation(self, is_reversed=False):
+        self.is_left = is_reversed
         self.index_0, self.index_of_hero_static_img = 0, 0
         self.image = pygame.transform.scale(load_image(f"images/entities/player/run/"
                                             f"{self.index_of_hero_running_img}.png", -1, is_reversed), (70, 90))
@@ -51,4 +53,7 @@ class Hero(pygame.sprite.Sprite):
             self.do_the_running_animation()
             self.rect.top -= self.dy
         else:
-            self.do_the_static_animation()
+            if self.is_left is True:
+                self.do_the_static_animation(is_reversed=True)
+            else:
+                self.do_the_static_animation()
