014597f7c3061b47bf2ec3d69ce2806333813430
Nikita <catnickit@yandex.ru>
2024-01-14 23:57:50 +0300

    Сохранил изменеия в файл level

---
+++ game_scripts/level.py
@@ -0,0 +1,25 @@
+from CharactersPhysics import Hero
+
+from animations import Camera
+
+from collide_system import Boarders
+
+from image_loader import load_image
+
+from outsiders_objects import Clouds, Particles
+
+import pygame
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, screen, sprite, all_sprites):
+        super().__init__(sprite, all_sprites)
+        self.screen = screen
+        self.size_block = [16, 16]
+        self.block = pygame.transform.scale(load_image('images/tiles/grass/0.png'), (self.size_block[0] * 2, self.size_block[1] * 2))
+        self.block_rect = self.block.get_rect()
+        self.block_rect.x, self.block_rect.y = self.screen.get_width() // 2 + 100, self.screen.get_height() // 2 + 25
+
+    # def platfoms(self):
+    #     hero = Hero(self.screen, self.hero_sprite, self.all_sprites)
+    #     if self.block_rect.collidepoint():
