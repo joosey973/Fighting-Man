8a60817fbe4e5543ffb6864d89edd5f966415cbe
Artemy <artemy.jevlak@yandex.ru>
2024-01-21 00:43:53 +0300

    Изменил класс Tilemap: Добавил аргументы в конструктор класса Tilemap Добавил фикисирование позиции tile спрайта

--- game_scripts/tilemap.py
+++ game_scripts/tilemap.py
@@ -1,13 +1,11 @@
-import json
-
 from image_loader import load_image
 
 import pygame
 
 
 class Tilemap(pygame.sprite.Sprite):
-    def __init__(self, ...):
-        super().__init__(...)
-        self.image = load_image(..., -1)
+    def __init__(self, pos_x, pos_y, type_of_tile, varint_of_tile, sprite_group):
+        super().__init__(sprite_group)
+        self.image = load_image(f"images/tiles/{type_of_tile}/{varint_of_tile}.png", -1)
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = ..., ...
+        self.rect.x, self.rect.y = pos_x, pos_y
