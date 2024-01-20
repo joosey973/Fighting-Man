3efefb31464c19fdfa935f6c3c308b60830c23fb
Artemy <artemy.jevlak@yandex.ru>
2024-01-21 00:30:03 +0300

    Исправил класс Tilemap

--- game_scripts/tilemap.py
+++ game_scripts/tilemap.py
@@ -1,52 +1,13 @@
 import json
 
+from image_loader import load_image
+
 import pygame
 
 
-NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
-PHYSICS_TILES = {'grass', 'stone'}
-
-
-class Tilemap():
-    def __init__(self, game, tile_size=16):
-        self.game = game
-        self.tile_size = tile_size
-        self.tilemap = {}
-        self.offgrid_tiles = []
-    
-    def tiles_around(self, pos):
-        tiles = []
-        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
-        # коллизия
-        for offset in NEIGHBOR_OFFSETS:
-            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
-            if check_loc in self.tilemap:
-                tiles.append(self.tilemap[check_loc])
-        return tiles
-    
-    def load(self, path):
-        f = open(path, 'r')
-        map_data = json.load(f)
-        f.close()
-        
-        self.tilemap = map_data['tilemap']
-        self.tile_size = map_data['tile_size']
-        self.offgrid_tiles = map_data['offgrid']
-
-    def physics_rects_around(self, pos):
-        rects = []
-        for tile in self.tiles_around(pos):
-            if tile['type'] in PHYSICS_TILES:
-                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
-        return rects
-    
-    def render(self, surf, offset=(0, 0)):
-        for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
-            
-        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
-            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
-                loc = str(x) + ';' + str(y)
-                if loc in self.tilemap:
-                    tile = self.tilemap[loc]
-                    surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+class Tilemap(pygame.sprite.Sprite):
+    def __init__(self, ...):
+        super().__init__(...)
+        self.image = load_image(..., -1)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = ..., ...
